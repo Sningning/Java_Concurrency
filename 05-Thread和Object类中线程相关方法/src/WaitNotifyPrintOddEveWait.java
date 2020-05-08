5b5265f9eaef1bd13b84c5ac8216b807030a209c
Song Ningning <qfsnn1117@gmail.com>
2020-05-08 22:19:44 +0800

    Demo: WaitNotifyPrintOddEveWait

--- 05-Thread和Object类中线程相关方法/src/WaitNotifyPrintOddEveWait.java
+++ 05-Thread和Object类中线程相关方法/src/WaitNotifyPrintOddEveWait.java
@@ -1,5 +1,3 @@
-import com.sun.deploy.ref.DeployJRE;
-
 /**
  * 两个线程交替打印0~100的奇偶数，用wait和notify
  *
@@ -9,7 +7,7 @@ import com.sun.deploy.ref.DeployJRE;
 public class WaitNotifyPrintOddEveWait {
 
     private static int count = 0;
-    private static Object obj = new Object();
+    private static final Object OBJ = new Object();
 
     public static void main(String[] args) {
 
@@ -22,14 +20,14 @@ public class WaitNotifyPrintOddEveWait {
         @Override
         public void run() {
             while (count <= 100) {
-                synchronized (obj) {
+                synchronized (OBJ) {
                     // 唤醒其他线程
-                    obj.notify();
+                    OBJ.notify();
                     System.out.println(Thread.currentThread().getName() + " : " + count++);
                     if (count <= 100) {
                         try {
                             // 如果任务还未结束，就休眠并释放锁
-                            obj.wait();
+                            OBJ.wait();
                         } catch (InterruptedException e) {
                             e.printStackTrace();
                         }
