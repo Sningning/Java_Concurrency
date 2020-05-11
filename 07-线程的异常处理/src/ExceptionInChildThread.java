f98f81bf19d9092865ed74b6f7848402f2936924
Song Ningning <qfsnn1117@gmail.com>
2020-05-11 10:28:39 +0800

    Demo: ExceptionInChildThread

---
+++ 07-线程的异常处理/src/ExceptionInChildThread.java
@@ -0,0 +1,20 @@
+/**
+ * 单线程，抛出，处理，有异常堆栈 多线程，子线程发生异常，会有什么不同？
+ *
+ * @Author: Song Ningning
+ * @Date: 2020-05-11 10:12
+ */
+public class ExceptionInChildThread implements Runnable {
+
+    public static void main(String[] args) {
+        new Thread(new ExceptionInChildThread()).start();
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i);
+        }
+    }
+
+    @Override
+    public void run() {
+        throw new RuntimeException();
+    }
+}
